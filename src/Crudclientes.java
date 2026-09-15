be99434874400ba422a485bf58dafbc80659864e
Saulo Sousa <saulo.sousa.miranda@gmail.com>
2026-09-15 20:39:36 -0300

    havia esquecido de adicionar public logo na setima linha do codigo (minha IDE nao completava essa parte)

--- src/Crudclientes.java
+++ src/Crudclientes.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Crudclientes {
     static ArrayList<Cliente> clientes = new ArrayList<>();
 
-    static void main(String[] args) {
+   public static void main(String[] args) {
         cadastrar();
         listar();
         System.out.println("----atualizando----");
